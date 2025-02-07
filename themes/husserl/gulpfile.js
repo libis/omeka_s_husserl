e4415c6487b7d281578e8dc614302812be875fee
yahnodewit-libis <yahno.dewit@kuleuven.be>
2025-02-07 14:19:58 +0100

    gulp

---
+++ themes/husserl/gulpfile.js
@@ -0,0 +1,35 @@
+'use strict'; 
+
+const gulp = require('gulp');
+
+//sass
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const sassGlob = require('gulp-sass-glob');
+
+//js
+const uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+
+gulp.task('buildingStyles', function () {
+    return gulp.src('./asset/sass/**/*.scss')
+        .pipe(sassGlob())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({}))
+        .pipe(gulp.dest('./asset/css'));
+});
+
+gulp.task('uglifyJs', function () {
+    return gulp.src('./asset/sass/**/*.js')
+        .pipe(uglify())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./asset/js'));
+});
+
+gulp.task('all:watch', function () {
+    gulp.watch('./asset/sass/**/*.scss', gulp.parallel('buildingStyles'));
+    gulp.watch('./asset/sass/**/*.js', gulp.parallel('uglifyJs'));
+});
+
+gulp.task('all:build', gulp.parallel(['buildingStyles', 'uglifyJs']));

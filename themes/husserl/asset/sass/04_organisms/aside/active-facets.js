87593012edb1c4a7aa1e139ca3c4bf0b2f132293
yahnodewit-libis <yahno.dewit@kuleuven.be>
2025-03-19 16:42:08 +0100

    delete comments

--- themes/husserl/asset/sass/04_organisms/aside/active-facets.js
+++ themes/husserl/asset/sass/04_organisms/aside/active-facets.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.facet-active-value').forEach(function(link) {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); // Voorkom de standaard actie
+            event.preventDefault();
+            localStorage.setItem('openPopUp', 'true');
             const facetName = this.dataset.facetName;
             const facetValue = this.dataset.facetValue;
             console.log(facetName);
@@ -9,9 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
             const form = document.getElementById('form-facets');
             const checkboxes = form.querySelectorAll(`input[name="${facetName}"][value="${facetValue}"]`);
             checkboxes.forEach(function(checkbox) {
-                checkbox.checked = false; // Deselecteer de checkbox
+                checkbox.checked = false; 
             });
-            form.submit(); // Dien het formulier opnieuw in
+            form.submit(); 
         });
     });
 });
